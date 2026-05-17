c6b4041acfdf18afa0b6ff1cc65eda1e6af0690e
SImon QUe <ques86225@gmail.com>
2026-05-18 00:55:11 +0800

    start queues

--- lesson_03/main/lesson_03.c
+++ lesson_03/main/lesson_03.c
@@ -8,11 +8,12 @@
 // Needed for atoi()
 #include <stdlib.h>
 
-/**
- * Using FreeRTOS, create two separate tasks. One listens for an integer over UART (from the Serial Monitor)
- * and sets a variable when it sees an integer. The other task blinks the onboard LED (or other connected LED)
- * at a rate specified by that integer. In effect, you want to create a multi-threaded system that allows for
- * the user interface to run concurrently with the control task (the blinking LED).
+/*
+Challenge:
+Using FreeRTOS, create two separate tasks. One listens for an integer over UART (from the Serial Monitor)
+and sets a variable when it sees an integer. The other task blinks the onboard LED (or other connected LED)
+at a rate specified by that integer. In effect, you want to create a multi-threaded system that allows for
+the user interface to run concurrently with the control task (the blinking LED).
  */
 static const char *LED_TAG = "LED TEST";
 static const char *UART_TAG = "UART TEST";
